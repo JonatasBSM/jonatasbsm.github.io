d2dd95ad166bc5b96eb1a22d59177db32f60f586
JonatasBSM <jonatasbsmcarvalho@gmail.com>
2023-08-26 13:10:18 -0300

    refazendo projetc section

--- script.js
+++ script.js
@@ -1,57 +1,5 @@
-// Get the modal element
-var modal = document.getElementById("modal");
 
-// Get the <span> element that closes the modal
-var closeBtn = document.querySelector("#modal .close");
 
-// Get all custom boxes inside the projects section
-var projectCustomBoxes = document.querySelectorAll("#projects .custom-box");
-
-// Add click event listener to each project custom box
-projectCustomBoxes.forEach(function(customBox) {
-  customBox.addEventListener("click", openModal);
-});
-
-// Function to open the modal
-function openModal(event) {
-  // Prevent event bubbling from skill custom boxes
-  event.stopPropagation();
-
-  // Get the video player element and resume element inside the modal
-  var videoPlayer = document.querySelector("#modal .video-player video");
-  var resume = document.querySelector("#modal .resume");
-
-  // Get the video URL and resume content from the clicked custom box
-  var videoURL = this.dataset.video;
-  var resumeContent = this.dataset.resume;
-
-  // Set the source of the video player
-  videoPlayer.src = videoURL;
-
-  // Set the resume content
-  resume.innerHTML = resumeContent;
-
-  // Display the modal
-  modal.style.display = "block";
-}
-
-// Function to close the modal
-function closeModal() {
-  // Hide the modal
-  modal.style.display = "none";
-
-  // Pause the video player
-  var videoPlayer = document.querySelector("#modal .video-player video");
-  videoPlayer.pause();
-}
-
-// Add click event listener to the close button
-
-window.addEventListener('click', (event) => {
-  if (event.target === modal) {
-      closeModal()
-  }
-});
 
 const sections = document.querySelectorAll("nav, section")
 
@@ -76,6 +24,29 @@ function handleScroll(event) {
 // Attach the scroll event listener to the document
 document.addEventListener('wheel', handleScroll, { passive: false });
 
+const projectContainers = document.querySelectorAll('.project-home, .teambuilder-container, .mbya-container');
+const prevProjectBtns = document.querySelectorAll('.prevProjectBtn');
+const nextProjectBtns = document.querySelectorAll('.nextProjectBtn');
+const projectsSection = document.getElementById('projects');
+let currentProjectIndex = 0;
+
+function navigateToProject(index) {
+  currentProjectIndex = Math.max(Math.min(index, projectContainers.length - 1), 0);
+  scrollToCurrentProject();
+}
+
+function scrollToCurrentProject() {
+  const container = projectContainers[currentProjectIndex];
+  projectsSection.scrollLeft = container.offsetLeft;
+}
+
+prevProjectBtns.forEach(button => {
+  button.addEventListener('click', () => navigateToProject(currentProjectIndex - 1));
+});
+
+nextProjectBtns.forEach(button => {
+  button.addEventListener('click', () => navigateToProject(currentProjectIndex + 1));
+});
 
 function typewriterEffect(element, text, speed) {
   let i = 0;
